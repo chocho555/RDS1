fae5c6fa9ac2fe9161b2c6b649bc806e7e3a66c1
chocho555 <chohannah0104@gmail.com>
2026-04-27 18:32:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const columns = document.querySelectorAll('.column');
 
 const images = [
-  'JPG/img1.jpg',
-  'JPG/img2.jpg',
-  'JPG/img3.jpg',
-  'JPG/img4.jpg',
-  'JPG/img5.jpg',
-  'JPG/img6.jpg'
+  './JPG/img1.JPG',
+  './JPG/img2.JPG',
+  './JPG/img3.JPG',
+  './JPG/img4.JPG',
+  './JPG/img5.JPG',
+  './JPG/img6.JPG'
 ];
 
-// 각 칸에 들어갈 이미지 개수
 const layout = [1, 0, 2, 3, 0, 1];
 
 let imgIndex = 0;
@@ -21,16 +20,18 @@ columns.forEach((column, colIndex) => {
     const box = document.createElement('div');
     box.classList.add('img-box');
 
-    // 🔥 세로 위치 랜덤 (간격 다양하게)
     box.style.marginTop = `${80 + Math.random() * 300}px`;
     box.style.marginBottom = `${80 + Math.random() * 200}px`;
 
     const img = document.createElement('img');
-
-    // 이미지 순서대로 사용 (겹치지 않게)
     img.src = images[imgIndex % images.length];
+
     imgIndex++;
 
+    box.appendChild(img);
+    column.appendChild(box);
+  }
+});
     box.appendChild(img);
     column.appendChild(box);
   }
