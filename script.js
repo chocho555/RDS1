e5dd560da54979226e1d7d60a3643d1434146463
chocho555 <chohannah0104@gmail.com>
2026-04-27 18:54:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,16 +20,30 @@ columns.forEach((column, colIndex) => {
     const box = document.createElement('div');
     box.classList.add('img-box');
 
-    box.style.marginTop = `${80 + Math.random() * 300}px`;
-    box.style.marginBottom = `${80 + Math.random() * 200}px`;
-
     const img = document.createElement('img');
     img.src = images[imgIndex % images.length];
-    img.alt = `image ${imgIndex + 1}`;
+    imgIndex++;
+
+    // 🔥 4번째 칸 (index 3)만 특별하게 처리
+    if (colIndex === 3) {
+      // 각 이미지마다 간격 다르게
+      if (i === 0) {
+        box.style.marginTop = '200px';
+        box.style.marginBottom = '120px';
+      } else if (i === 1) {
+        box.style.marginTop = '350px';
+        box.style.marginBottom = '180px';
+      } else if (i === 2) {
+        box.style.marginTop = '500px';
+        box.style.marginBottom = '220px';
+      }
+    } else {
+      // 나머지 칸은 기존 랜덤
+      box.style.marginTop = `${80 + Math.random() * 300}px`;
+      box.style.marginBottom = `${80 + Math.random() * 200}px`;
+    }
 
     box.appendChild(img);
     column.appendChild(box);
-
-    imgIndex++;
   }
 });
