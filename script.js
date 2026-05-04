6aeed9db4d89268b32a4075e6aa63d48b6ffab2b
chocho555 <chohannah0104@gmail.com>
2026-05-05 00:32:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const leftPanel = document.querySelector('.left');
 const rightPanel = document.querySelector('.right');
 
-const gapWidth = 80; // 이미지가 보이는 틈 크기
-let leftRatio = 0.18; // 처음 왼쪽 판넬 비율
-let targetRatio = 0.18;
+const gapWidth = 20;
+
+let leftRatio = 0.5;
+let targetRatio = 0.5;
 
 function updatePanels() {
   const totalWidth = window.innerWidth;
   const usableWidth = totalWidth - gapWidth;
 
-  // 부드럽게 따라가는 모션
-  leftRatio += (targetRatio - leftRatio) * 0.06;
+  leftRatio += (targetRatio - leftRatio) * 0.008;
 
   const leftWidth = usableWidth * leftRatio;
   const rightWidth = usableWidth - leftWidth;
@@ -22,11 +22,11 @@ function updatePanels() {
 }
 
 leftPanel.addEventListener('mouseenter', () => {
-  targetRatio = 0.75; 
+  targetRatio = 0.995;
 });
 
 rightPanel.addEventListener('mouseenter', () => {
-  targetRatio = 0.15;
+  targetRatio = 0.005;
 });
 
 window.addEventListener('resize', updatePanels);
