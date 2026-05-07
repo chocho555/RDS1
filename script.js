3aa81a0a6b428b2b55ff1c8186d70f634d18a7b5
chocho555 <chohannah0104@gmail.com>
2026-05-08 01:23:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-const images = [
-  "images/img1.jpg",
-  "images/img2.jpg",
-  "images/img3.jpg",
-  "images/img4.jpg",
-  "images/img5.jpg"
-];
-
-const bgImage = document.getElementById("bgImage");
-const slitImage = document.getElementById("slitImage");
-const slitWrapper = document.querySelector(".slit-wrapper");
-
-let index = 0;
-
-setInterval(() => {
-  index = (index + 1) % images.length;
-  bgImage.src = images[index];
-  slitImage.src = images[index];
-}, 1000);
-
 window.addEventListener("scroll", () => {
-  const maxScroll = document.body.scrollHeight - window.innerHeight;
-  const progress = Math.min(window.scrollY / maxScroll, 1);
 
-  const eased = progress * progress * (3 - 2 * progress);
+  const maxScroll =
+    document.body.scrollHeight - window.innerHeight;
 
-  const minSlitWidth = 6;
-  const maxSlitWidth = window.innerWidth * 0.09; // 최대 폭
-  const slitWidth = minSlitWidth + (maxSlitWidth - minSlitWidth) * eased;
+  const progress =
+    Math.min(window.scrollY / maxScroll, 1);
 
-  const minSlitHeight = 150;
-  const maxSlitHeight = window.innerHeight;
-  const slitHeight = minSlitHeight + (maxSlitHeight - minSlitHeight) * eased;
+  const eased =
+    1 - Math.pow(1 - progress, 3);
 
-  const imageScale = 1 + eased * 0.8;
-  const blur = 4 - eased * 4;
+  /* Z축 이동 */
 
-  slitWrapper.style.width = `${slitWidth}px`;
-  slitWrapper.style.height = `${slitHeight}px`;
-  slitWrapper.style.transform = `translate(-50%, -50%) scale(1)`;
+  const zMove =
+    -1200 + eased * 1150;
+
+  /* 세로 길이 */
+
+  const height =
+    120 + eased * window.innerHeight;
+
+  slitWrapper.style.height =
+    `${height}px`;
+
+  slitWrapper.style.transform =
+    `
+    translate(-50%, -50%)
+    translateZ(${zMove}px)
+    `;
+
+  /* 이미지 확대 */
+
+  const imageScale =
+    1 + eased * 0.5;
 
   slitImage.style.transform =
-    `translate(-50%, -50%) scale(${imageScale})`;
-
-  slitWrapper.style.filter = `blur(${blur}px)`;
+    `
+    translate(-50%, -50%)
+    scale(${imageScale})
+    `;
 });
