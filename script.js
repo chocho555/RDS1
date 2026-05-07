41b085fee3f06686f693bb8a66cb476362f2b729
chocho555 <chohannah0104@gmail.com>
2026-05-08 01:40:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const images = [
+  "images/img1.jpg",
+  "images/img2.jpg",
+  "images/img3.jpg",
+  "images/img4.jpg",
+  "images/img5.jpg"
+];
+
+const bgImage = document.getElementById("bgImage");
+const slitImage = document.getElementById("slitImage");
+const slitWrapper = document.querySelector(".slit-wrapper");
+
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  bgImage.src = images[index];
+  slitImage.src = images[index];
+}, 1000);
+
 window.addEventListener("scroll", () => {
   const maxScroll = document.body.scrollHeight - window.innerHeight;
   const progress = Math.min(window.scrollY / maxScroll, 1);
   const eased = 1 - Math.pow(1 - progress, 3);
 
   const scale = 1 + eased * 18;
-  const panelMove = eased * 4.5; // 최대 틈 폭 느낌 조절
+  const panelMove = eased * 4.5;
 
   slitWrapper.style.transform =
     `translate(-50%, -50%) scale(${scale})`;
