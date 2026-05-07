01dee2944b45e20d0ed230c92adbd5b85f8e4eef
chocho555 <chohannah0104@gmail.com>
2026-05-08 01:01:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,53 +8,22 @@ const images = [
 
 const bgImage = document.getElementById("bgImage");
 const slitImage = document.getElementById("slitImage");
+const slitWrapper = document.querySelector(".slit-wrapper");
 
 let index = 0;
 
-/* 이미지 변경 */
-
 setInterval(() => {
-
   index = (index + 1) % images.length;
-
   bgImage.src = images[index];
   slitImage.src = images[index];
-
 }, 1000);
 
-
-/* 거리감 */
-
 window.addEventListener("scroll", () => {
+  const maxScroll = document.body.scrollHeight - window.innerHeight;
+  const progress = window.scrollY / maxScroll;
 
-  const scrollY = window.scrollY;
+  const scale = 1 + progress * 18;
 
-  /* 틈 넓이 */
-
-  const gap = 6 + scrollY * 0.15;
-
-  document.querySelector(".slit").style.width =
-    `${gap}px`;
-
-  /* 틈 높이 */
-
-  const height = 30 + scrollY * 0.08;
-
-  document.querySelector(".slit").style.height =
-    `${height}vh`;
-
-  /* 위치 */
-
-  const top = 35 - scrollY * 0.04;
-
-  document.querySelector(".slit").style.top =
-    `${top}vh`;
-
-  /* 이미지 확대 */
-
-  const scale = 1 + scrollY * 0.0005;
-
-  slitImage.style.transform =
+  slitWrapper.style.transform =
     `translate(-50%, -50%) scale(${scale})`;
-
 });
